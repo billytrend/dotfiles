8b39b2e30d1c27e30a6e1f522b88a05980cafc1c
billytrend <billy.trend@gmail.com>
2022-01-25 14:44:55 +0000

    Add brew and gng

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ echo "--Starting personalize"
 sudo apt-get update
 sudo apt-get install -y fish
 sudo chsh -s /usr/bin/fish $USER
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap gdubw/gng
+brew install gng
