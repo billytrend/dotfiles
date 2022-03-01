5c995613c1f16c96121a09eb0e9db0613c50a111
billytrend <billy.trend@gmail.com>
2022-03-01 17:30:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,8 @@ sudo chsh -s /usr/bin/fish $USER
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew tap gdubw/gng
 brew install gng
